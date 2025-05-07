4f79170c22821c8f040d5a57d92b305c78356774
Trung Anh <nthao0844@gmail.com>
2025-05-07 17:29:12 +0700

    Update Json.swift

--- Sources/Logto/Types/Json.swift
+++ Sources/Logto/Types/Json.swift
@@ -16,3 +16,38 @@ public enum JsonValue: Codable, Equatable {
 }
 
 public typealias JsonObject = [String: JsonValue]
+
+extension JsonValue {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let string = try? container.decode(String.self) {
+            self = .string(string)
+        } else if let number = try? container.decode(Double.self) {
+            self = .number(number)
+        } else if let bool = try? container.decode(Bool.self) {
+            self = .bool(bool)
+        } else if let array = try? container.decode([JsonValue].self) {
+            self = .array(array)
+        } else if let object = try? container.decode([String: JsonValue].self) {
+            self = .object(object)
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid JsonValue")
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let value):
+            try container.encode(value)
+        case .number(let value):
+            try container.encode(value)
+        case .bool(let value):
+            try container.encode(value)
+        case .array(let value):
+            try container.encode(value)
+        case .object(let value):
+            try container.encode(value)
+        }
+    }
+    
